255c92269d1798acd9a393f77ec17de15918c8c4
Leon <thelightofking@sina.com>
2018-07-08 17:11:50 -0400

    Add existing file

---
+++ WRDS_python_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 26 11:24:26 2018
+
+@author: thelightofking
+"""
+
+import wrds
+#qiaozhih
+#Wrds2018
+#Tutor:https://wrds-www.wharton.upenn.edu/pages/support/accessing-wrds-remotely/accessing-wrds-remotely-python/python-code-examples-workflow-remote/
+db = wrds.Connection()
+def return_comp(long_string):
+    temp= long_string.split(",")
+    return db.get_table('comp', 'funda', columns=temp, obs=1000)
+    
+
+#company=db.get_table('comp', 'company').head()
+
+
+#/*firm variables*/
+#/*income statement*/
+Firm_income=return_comp('sale,revt,cogs,xsga,dp,xrd,xad,ib,ebitda,ebit,nopi,spi,pi,txp,ni,txfed,txfo,txt,xint')
+#/*CF statement and others*/
+cashflow=return_comp('capx,oancf,dvt,ob,gdwlia,gdwlip,gwo')
+#/*assets*/
+asset=return_comp('rect,act,che,ppegt,invt,at,aco,intan,ao,ppent,gdwl,fatb,fatl')
+#/*liabilities*/
+liability=return_comp('lct,dlc,dltt,lt,dm,dcvt,cshrc,dcpstk,pstk,ap,lco,lo,drc,drlt,txdi')
+#/*equity and other*/
+equity=return_comp('ceq,scstkc,emp,csho')
+#/*market*/
+market=return_comp('abs(prcc_f),csho,prcc_f')
